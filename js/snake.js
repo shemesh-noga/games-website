35cb3e3cba18ff2947ca70b33321b2ec3613b4be
shemesh-noga <nogashemesh@gmail.com>
2024-09-30 12:59:55 +0300

    snake works before eating

--- js/snake.js
+++ js/snake.js
@@ -1,10 +1,74 @@
 let snake = document.getElementById("snake");
-let posX = 70
-snake.style.left = posX + "px";
+let snakeBoard = document.getElementById("snakeBoard")
+let posX = 0;
+let posY = 0;
+let direction = "right";
+var speed = "slow";
+var speedVal = 550;
+let speeds = document.getElementById("speeds");
+let snakeSize = 3;
 
-function changeX(){
-    posX += 35
-    return snake.style.left = posX + "px";
+// speeds.addEventListener("click", () => {
+//     speeds.addEventListener("change", () => {
+//       changeSpeed();
+//     });
+// });
+
+// function changeSpeed() {
+//     var speed = document.getElementById("speeds").value;
+//     if (speed == "slow") {speedVal = 550}
+//     if (speed == "medium") {speedVal = 300}
+//     if (speed == "hard") {speedVal = 200}
+//     if (speed == "mega") {speedVal = 50}
+//     return speedVal;
+// }
+
+
+
+addEventListener("keydown", (e) => {
+    if (e.key == "ArrowRight") {
+        direction = "right"
+    }
+    if (e.key == "ArrowLeft") {
+        direction = "left"
+    }
+    if (e.key == "ArrowUp") {
+        direction = "up"
+    }
+    if (e.key == "ArrowDown") {
+        direction = "down"
+    }
+})
+
+
+function checkDirection() {
+    if (direction === "right") {
+        posX += 35
+    }
+    if (direction === "left") {
+        posX -= 35
+    }
+    if (direction === "up") {
+        posY -= 35
+    }
+    if (direction === "down") {
+        posY += 35
+    }
 }
 
-setInterval(changeX, 300)
+function changePos(){
+    checkDirection()
+    snake.style.left = posX + "px";
+    snake.style.top = posY + "px";
+}
+
+
+// creates the snake body
+for(let j = 0 ; j < snakeSize ; j++){
+    const div = document.createElement("div");
+    snakeBoard.appendChild(div);
+}
+
+
+// setInterval(changePos, speedVal);
+
